4c1f1ba5805dd27ac2d316858c7bd18f4842d383
Camelia Ciuca <camelia1414@gmail.com>
2024-08-01 13:43:06 +0200

    App file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//rce
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>MarketPlace App</div>
+    )
+  }
+}
+
+export default App
